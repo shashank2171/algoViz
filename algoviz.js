39f2a85d8aeed7382823af818b547743af018e87
shashank2171 <103600766+shashank2171@users.noreply.github.com>
2022-08-12 01:12:48 +0530

    Add files via upload

--- algoviz.js
+++ algoviz.js
@@ -1,4 +1,5 @@
 var arr = [1,8,4,3,5,7,5,3,6,8,23,34,2,1,4];
+var l = arr.length;
 
 var max = 0;
 for(var i =0 ; i<arr.length; i++){
@@ -17,16 +18,18 @@ arr.forEach(function(e){
     li.innerText = e;
     li.style.width=(e/max*100)+"%";
     li.style.backgroundColor = "lightgrey";
+    li.style.borderTopRightRadius= "10px";
+    li.style.borderBottomRightRadius= "10px";
     x.style.margin = "5px";
     bars.append(x);
     document.getElementById("d_"+c).append(li);
     c+=1;
 });
 
-async function sort(){
-    var l = arr.length;
-    for(var i=0; i<arr.length; i++){
-        for(var j=0; j<arr.length-i-1; j++)
+async function bubbleSort(){
+    
+    for(var i=0; i<l; i++){
+        for(var j=0; j<l-i-1; j++)
         {   var curr = document.getElementById(j);
             var curr2 = document.getElementById(j+1);
             curr.style.backgroundColor = "blue";
@@ -37,25 +40,64 @@ async function sort(){
                 arr[j] = arr[j+1];
                 
                 curr.innerText=arr[j];
-                
-                console.log((arr[j]/max*100)+"%");
-                
-            
+
                 arr[j+1] = temp;
                 
                 curr2.innerText=arr[j+1];
                 
-                
+                curr.style.width = document.getElementById(j).innerText/max*100+"%";
+                curr2.style.width = document.getElementById(j+1).innerText/max*100+"%";
                 
                 
             }
             await new Promise(done => setTimeout(() => done(), 200));
             curr.style.backgroundColor = 'lightgrey';
-            curr2.style.backgroundColor = 'lightgrey';
-            curr.style.width = document.getElementById(j).innerText/max*100+"%";
-            curr2.style.width = document.getElementById(j+1).innerText/max*100+"%";
+            
+            
         }
         document.getElementById(l-1).style.backgroundColor = "green";
         l-=1;
     }
 }
+
+async function insertionSort(){
+
+    for(var i=1; i<l; i++){
+
+        var t = arr[i];
+        var j = i-1;
+
+        var curr = document.getElementById(i);
+        curr.style.backgroundColor = "blue";
+        await new Promise(done => setTimeout(() => done(), 500));
+        while (j >= 0 && arr[j] > t) {
+
+            curr2 = document.getElementById(j+1);
+            arr[j + 1] = arr[j];
+            curr2.innerText = arr[j];
+            curr2.style.width = document.getElementById(j).innerText/max*100+"%";
+            j = j - 1;
+            
+                  
+            
+        }
+        curr2 = document.getElementById(j+1);
+        curr2.innerText=t;
+        curr2.style.backgroundColor = "red";
+        curr2.style.width = document.getElementById(j+1).innerText/max*100+"%";
+        await new Promise(done => setTimeout(() => done(), 500)); 
+        arr[j + 1] = t;
+        curr.innerText = arr[i];
+        curr.style.width = document.getElementById(i).innerText/max*100+"%";
+        curr.style.backgroundColor = 'lightgrey';
+        curr2.style.backgroundColor = 'lightgrey';
+        
+    }
+
+    for(var i=0; i<l; i++){
+        document.getElementById(i).style.backgroundColor="green";
+        await new Promise(done => setTimeout(() => done(), 50));
+    }
+}
+
+console.log(arr);
